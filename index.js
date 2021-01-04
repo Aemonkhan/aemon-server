65a3f9da6dd143540ea18a6ab6ce196e731e5cd5
nomanali630 <hussainkhaan78@gmail.com>
2021-01-04 12:53:29 +0500

    work

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 4000;
+app.use(bodyParser.urlencoded({extended:true}));
 app.listen(PORT,(req,res)=>{
     console.log('server is running at PORT', PORT)
 })
@@ -19,3 +21,9 @@ app.listen(PORT,(req,res)=>{
 // })
 app.use(express.static(path.join(__dirname,'public')))
 app.use(express.static(path.join(__dirname,'registration')))
+app.get('/signup',(req,res)=>{
+    res.sendFile(path.join(__dirname,'registration','signup.html'))
+})
+app.post('/signup',(req,res)=>{
+    res.send(req.body)
+})
