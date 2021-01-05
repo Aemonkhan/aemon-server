4a20031acfe398b182ab99547d8bafca2d729906
nomanali630 <hussainkhaan78@gmail.com>
2021-01-05 12:55:45 +0500

    appwork

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.listen(PORT,(req,res)=>{
     console.log('server is running at PORT', PORT)
 })
+let users =[
+    {name:'Faiza',id:1, email:'faz.pak@gmail.com',password:'12345'},
+    {name:'Kulsoom',id:1, email:'Kusloom@gmail.com',password:'11111'}
+]
 // app.get('/',(req,res)=>{
 //     // res.send('<h1>My first App</h1>')
 //     res.sendFile(path.join(__dirname,'public','index.html'))
@@ -24,6 +28,33 @@ app.use(express.static(path.join(__dirname,'registration')))
 app.get('/signup',(req,res)=>{
     res.sendFile(path.join(__dirname,'registration','signup.html'))
 })
+app.get('/signin',(req,res)=>{
+    res.sendFile(path.join(__dirname,'registration','signin.html'))
+})
 app.post('/signup',(req,res)=>{
-    res.send(req.body)
+    //  res.send(req.body)
+let {name,email,password}= req.body
+let found =users.some((item)=>item.email==email)
+if(found){
+res.send('<h1>User already existed</h1>')
+}
+else{
+    users.push ({name,email,password,id:users.length+1})
+    //  res.send('<h1>new user added</h1>')
+    //   res.sendFile(path.join(__dirname,'registration','signin.html'))
+    res.redirect('/signin')
+}
+})
+app.post('/signin',(req,res)=>{
+    let {name,email,password}=req.body
+    let found = users.some((item)=>item.password==password && item.email==email)
+//  console.log('found in signin',found)
+    if(found){
+        res.send('<h1> Welcome </h1>')
+
+    }
+    else{
+
+        res.redirect('/signin')
+    }
 })
